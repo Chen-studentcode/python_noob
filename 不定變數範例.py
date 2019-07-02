eadb8874c4f2eb248b9f6a77c521c7575dc179e8
卡森 <51984413+Chen-studentcode@users.noreply.github.com>
2019-07-02 14:18:18 +0800

    Add files via upload

---
+++ 不定變數範例.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 15:14:30 2019
+
+@author: User
+"""
+
+
+def test_args(*args):      #值或LIST
+    for i in args:
+        #print(i)
+        for j in i:
+            print(j)
+    
+a=[1,2,3]
+b=[4,5,6]
+test_args(a)
+print('*'*20)
+test_args(a,b)
+
+def test_kwargs(**kwargs):    #有鍵和值
+   for k, v in kwargs.items():
+      print('Optional argument %s (*kwargs): %s' % (k, v))
+      
+
+test_kwargs(a=3,b=4,d=5)
+
+
+
---
+++ 淺深拷貝差異.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from copy import deepcopy
+
+
+a=[1,2,3,[4,5]] #原始物件
+b=a[:]          #淺拷貝
+c=deepcopy(a)   #深拷貝
+
+a[0]='test'     #改變外層,只有a改變
+a[3][0]='test2' #改變內層,a和b都改變
+
+
