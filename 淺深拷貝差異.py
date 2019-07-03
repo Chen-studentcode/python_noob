ade0c95d13575df7d97f31f6dd4dd12e562479b1
卡森 <51984413+Chen-studentcode@users.noreply.github.com>
2019-07-03 15:34:47 +0800

    Update 淺深拷貝差異.py

--- 淺深拷貝差異.py
+++ 淺深拷貝差異.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 from copy import deepcopy
 
 
@@ -11,7 +6,11 @@ a=[1,2,3,[4,5]] #原始物件
 b=a[:]          #淺拷貝
 c=deepcopy(a)   #深拷貝
 
-a[0]='test'     #改變外層,只有a改變
+a[0]='test1'     #改變外層,只有a改變
+print(f'{a}\n{b}\n{c}')
+print('*'*20)
+
 a[3][0]='test2' #改變內層,a和b都改變
+print(f'{a}\n{b}\n{c}')
 
 
